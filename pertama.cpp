2c5bbe4d37b33c8136c1503dbda20fb702d54f47
khalid0931 <mutiarif53@gmail.com>
2026-02-23 15:42:15 +0700

    first commit

---
+++ pertama.cpp
@@ -0,0 +1,22 @@
+//contoh library
+#include <iostream>
+using namespace std;
+
+//variable global
+string nama;
+
+int main(){ //program uutama
+    
+    int age; //variable local
+    int age1; //variable local
+    age = 20;
+    age1 = 15;
+    
+
+
+    cout << "Halo Word" << endl;
+    cout << 99 << end;
+    cout << age;
+    cout << age1
+    
+}
