dcd088ef09fb1257642bab2bebfefb7230cc0444
khalid0931 <mutiarif53@gmail.com>
2026-02-23 16:39:37 +0700

    selesai

--- hitungluas.cpp
+++ hitungluas.cpp
@@ -5,11 +5,21 @@ int main(){ //mulai langkah
     int p,l, luas;
 
     //langkah 2
+    cout << "masukan panjang: ";
+    cin >> p;
 
+    //langkah 3
+    cout << "masukan lebar: ";
+    cin >> l;
 
+    //langkah 4
+    luas = p*l;
+
+    //langkah 5
+    cout << "hasilnya" << luas;
+    //selesai
 }
 
 
 
 
-}
